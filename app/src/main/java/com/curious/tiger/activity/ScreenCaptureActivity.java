fc1b5d40445064098193f6384d20faaf1c0f6484
kang36897 <kang36897@163.com>
2016-04-14 17:26:49 +0800

    1.begin to write the screen capture funtionality

---
+++ app/src/main/java/com/curious/tiger/activity/ScreenCaptureActivity.java
@@ -0,0 +1,18 @@
+package com.curious.tiger.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2016/4/14.
+ */
+public class ScreenCaptureActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
